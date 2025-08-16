f2184af02b745ec934f73532ecb76f6196516a78
Raiyan109 <kraiyan109@gmail.com>
2025-08-16 15:18:35 +0600

    password hashed with bcrypt

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,12 +1,20 @@
 
 import { IUser } from './user.interface';
 import * as UserModel from './user.model';
+import bcrypt from "bcrypt";
+
+const SALT_ROUNDS = 10;
 
 export const addUser = async (user: IUser) => {
-    
-    return await UserModel.createUser(user);
-};
+    // hash the password
+    const hashedPassword = await bcrypt.hash(user.password, SALT_ROUNDS);
 
+    // pass hashed password to DB
+    return await UserModel.createUser({
+        ...user,
+        password: hashedPassword,
+    });
+};
 export const listUsers = async () => {
     return await UserModel.getAllUsers();
 };

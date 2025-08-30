bb3052e6ff231b4138d0b7a286a455b1b130d89e
Raiyan109 <kraiyan109@gmail.com>
2025-08-30 16:17:57 +0600

    ok

--- dist/app/database/db.js
+++ dist/app/database/db.js
@@ -1,18 +1,30 @@
 "use strict";
+// import { Pool } from 'pg';
+// import config from '../config';
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// const pool = new Pool({
+//     user: config.DB_USER || 'postgres',
+//     host: config.DB_HOST || 'localhost',
+//     database: config.DB_NAME || 'mydb',
+//     password: config.DB_PASSWORD || 'password',
+//     port: Number(config.DB_PORT) || 5432,
+// });
+// pool.on('connect', () => {
+//     console.log('Connected to PostgreSQL');
+// });
+// export default pool;
 const pg_1 = require("pg");
 const config_1 = __importDefault(require("../config"));
 const pool = new pg_1.Pool({
-    user: config_1.default.DB_USER || 'postgres',
-    host: config_1.default.DB_HOST || 'localhost',
-    database: config_1.default.DB_NAME || 'mydb',
-    password: config_1.default.DB_PASSWORD || 'password',
-    port: Number(config_1.default.DB_PORT) || 5432,
+    connectionString: config_1.default.DB_URL, // use connection string from Neon
+    ssl: {
+        rejectUnauthorized: false, // Neon requires SSL in serverless envs like Vercel
+    },
 });
 pool.on('connect', () => {
-    console.log('Connected to PostgreSQL');
+    console.log('✅ Connected to PostgreSQL');
 });
 exports.default = pool;
